9fa13dfd349960e38b1dfced07152650db675979
Cleydyr Albuquerque <cleydyr@gmail.com>
2021-01-28 07:50:28 -0300

    Updates

---
+++ precache-manifest.674c4d97cb44794fe098b9ec9e89cfd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e49ecbd440b2f454720a01e3c0d2eb",
+    "url": "/expectation-simulator/index.html"
+  },
+  {
+    "revision": "bb015646fd3885ab8644",
+    "url": "/expectation-simulator/static/css/2.5eb8c94b.chunk.css"
+  },
+  {
+    "revision": "bb015646fd3885ab8644",
+    "url": "/expectation-simulator/static/js/2.f8389215.chunk.js"
+  },
+  {
+    "revision": "0ba89cab4ab770410db0",
+    "url": "/expectation-simulator/static/js/main.7eb045c0.chunk.js"
+  },
+  {
+    "revision": "7e87cecea51496c1e5ef",
+    "url": "/expectation-simulator/static/js/runtime~main.2ccc58f7.js"
+  }
+]);
